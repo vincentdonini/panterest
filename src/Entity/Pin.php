d1a8de6332b968a016ee6636535c9e72da3c7f83
Vincent DONINI <vincent.donini@maison.fr>
2023-08-14 14:48:05 +0200

    Add createdAt and updatedAt to pin entity

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -3,10 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\PinRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
+ * @ORM\Table(name="pin")
+ * @ORM\HasLifecycleCallbacks
  */
 class Pin
 {
@@ -26,6 +29,25 @@ class Pin
      * @ORM\Column(type="text")
      */
     private ?string $description = null;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private DateTimeImmutable $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private DateTimeImmutable $updatedAt;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable;
+        $this->updatedAt = new DateTimeImmutable;
+    }
 
     public function getId(): ?int
     {
@@ -54,5 +76,42 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if($this->getCreatedAt() === null){
+            $this->setCreatedAt(new DateTimeImmutable);
+        }
+
+        $this->setUpdatedAt(new DateTimeImmutable);
     }
 }
