eeddd90b92cfd5c5a89eb7d16429152e0110122d
Vincent DONINI <vincent.donini@maison.fr>
2023-08-14 17:11:26 +0200

    Add create and edit Pin form

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,15 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
+    private $manager;
+
+    public function __construct(
+        EntityManagerInterface $manager
+    ){
+        $this->manager = $manager;
+    }
+
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods={"GET"})
+     * @Route("/pins/", name="app_home", methods={"GET"})
      */
     public function index(
         PinRepository $pinRepository
@@ -25,10 +37,32 @@ class PinsController extends AbstractController
     }
 
     /**
-     * @Route("/pins/{id}", name="app_pins_details")
+     * @Route("/pins/create", name="app_pins_create", methods={"GET", "POST"})
+     */
+    public function create(
+        Request $request
+    ): Response
+    {
+        $pin = new Pin;
+        $form = $this->createForm(PinType::class, $pin);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->manager->persist($pin);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_details", methods={"GET"})
      */
     public function details(
-        PinRepository $pinRepository,
         Pin $pin
     ): Response
     {
@@ -36,4 +70,30 @@ class PinsController extends AbstractController
             'pin' => $pin,
         ]);
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "PUT"})
+     */
+    public function edit(
+        Request $request,
+        Pin $pin
+    ): Response
+    {
+        $form = $this->createForm(PinType::class, $pin, [
+            'method' => 'PUT'
+        ]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->manager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/edit.html.twig', [
+            'pin' => $pin,
+            'form' => $form->createView()
+        ]);
+    }
+
 }
