b65bab7627d17f1a5e63a71430a6cf8d43785049
Vincent DONINI <vincent.donini@maison.fr>
2023-08-18 16:52:34 +0200

    Add images to data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,9 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use function Symfony\Component\String\u;
 
 class AppFixtures extends Fixture
 {
@@ -19,19 +22,36 @@ class AppFixtures extends Fixture
     {
         $this->faker = Factory::create('fr_FR');
     }
+
     public function load(ObjectManager $manager): void
     {
+        $client = new Client();
 
         // Pins
-        $pins = [];
         for ($i = 1; $i <= 20; $i++) {
-            $pin = new Pin();
-            $pin
-                ->setTitle($this->faker->unique()->word())
-                ->setDescription($this->faker->text(300))
-                ->setImageName(null);
-            $pins[] = $pin;
-            $manager->persist($pin);
+            try {
+                $title = $this->faker->unique()->text(25);
+                $imageUrl = 'https://loremflickr.com/800/600';
+                $imageName = null;
+
+                $rand = mt_rand(0, 1);
+                if($rand){
+                    $response = $client->get($imageUrl);
+                    $content = $response->getBody()->getContents();
+                    $imageName = uniqid(u($title)->snake().'_', true) . '.jpg';
+                    file_put_contents('public/uploads/pins/' . $imageName, $content);
+                }
+
+                $pin = new Pin();
+                $pin
+                    ->setTitle($title)
+                    ->setDescription($this->faker->text(300))
+                    ->setImageName($imageName);
+
+                $manager->persist($pin);
+            } catch (GuzzleException $e) {
+                // Handle exception
+            }
         }
 
         $manager->flush();
