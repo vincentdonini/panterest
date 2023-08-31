3af9c69ebd63709d882d935eaaf22e2188e9cb12
Vincent DONINI <vincent.donini@maison.fr>
2023-08-31 15:42:54 +0200

    Fix verifyUserEmail redirections and flashMessages

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -97,9 +97,9 @@ class RegistrationController extends AbstractController
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+            $this->addFlash('error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_home');
         }
 
         $this->addFlash('success', 'Your email address has been verified.');
