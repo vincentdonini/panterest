04bd7c8a1831cbd7b73cb12356295bf26830a240
Vincent DONINI <vincent.donini@maison.fr>
2023-09-04 15:15:51 +0200

    Change reset password redirection

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -138,7 +138,7 @@ class ResetPasswordController extends AbstractController
 
             $this->addFlash('success', 'Password updated successfully.');
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('reset_password/reset.html.twig', [
